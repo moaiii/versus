a23c9d82ddae16e8604e3fdafd1f5ef7e3452cc3
chrismelville <chrismelville@skyscanner.net>
2019-06-11 08:24:23 +0100

    initial commit

---
+++ src/ui/components/organisms/StandingsTable/index.js
@@ -0,0 +1,35 @@
+// @flow
+
+import Home from "./Home.jsx";
+import store from '../../../utils/redux/store';
+import { connect } from "react-redux";
+import * as actions from './Home.actions';
+
+function mapStoreToProps(store) {
+  return {
+    countries: store.countriesReducer.value,
+    leagues: store.leaguesReducer.value,
+    seasons: store.datesReducer.seasons,
+    games: store.gamesReducer.games,
+    table: store.dataReducer.table,
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    getSeasons: () => dispatch(actions.getSeasons()),
+    getCountries: () => dispatch(actions.getCountries.submit()),
+    setSelectedCountry: country => dispatch(actions.setSelectedCountry(country)),
+    setSelectedLeague: league => dispatch(actions.setSelectedLeague(league)),
+    setDateSelection: dates => dispatch(actions.setDateSelection({
+        dates,
+        middlewareMode: "last"
+      })
+    ),
+  }
+}
+
+export default connect(
+  mapStoreToProps,
+  mapDispatchToProps
+)(Home);
