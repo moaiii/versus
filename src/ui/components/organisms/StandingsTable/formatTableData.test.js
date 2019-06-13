f04d6b6dfd96e5d8a97b940f7806b2e6a137e3b9
chrismelville <chrismelville@skyscanner.net>
2019-06-13 08:00:54 +0100

    added test

--- src/ui/components/organisms/StandingsTable/formatTableData.test.js
+++ src/ui/components/organisms/StandingsTable/formatTableData.test.js
@@ -2,6 +2,9 @@ import formatTableData from './formatTableData';
 
 describe('formatTableData', () => {
   it('should return an object with the required keys', () => {
+    const requiredKeys = ['Position', 'Club', 'Played', 'Won', 'Drawn',
+      'Lost', 'GF', 'GA', 'GD', 'Points'];
+
     expect(true).toBe(true);
   })
 })
