2c23ef5c4601cb6c1590ddd94775375d7fe90ee0
Chris Croal <ccroal@ntlworld.com>
2019-06-19 11:17:43 +0100

    inital commit added updated url

--- src/utils/network/config.js
+++ src/utils/network/config.js
@@ -1,2 +1,2 @@
 
-export const api_url = 'https://apifootball.com/api/?APIkey=6cd1d51f64cae5f83f7a35806b8eab1bcd5b93d7ddb3a8addecf9b1905c81607&';
+export const api_url = 'https://05osvsvx1l.execute-api.eu-west-1.amazonaws.com/latest?';
